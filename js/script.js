baa998b28df186166b3f4cd9febbca33a17c65d9
Alexander Little <AlexanderLittle@Alexs-MacBook-Pro-2015.local>
2023-03-04 20:09:38 +1300

    succesfully added dark mode

--- js/script.js
+++ js/script.js
@@ -50,3 +50,132 @@ function percentage() {
 }
 
 
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("cButton", "backButton");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("backButton");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("percentButton");
+  element.classList.toggle("darkMode");
+    var element = document.getElementById("7Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("8Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("9Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("4Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("5Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("6Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("1Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("2Button");
+        element.classList.toggle("darkMode");
+        var element = document.getElementById("3Button");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("periodButton");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("zero");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("divideOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("timesOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("minusOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("addOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("equaloOperator");
+  element.classList.toggle("darkModeEqual");
+  var element = document.getElementById("highlight");
+  element.classList.toggle("darkModeHighlight");
+  var element = document.getElementById("calculation");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("result");
+  element.classList.toggle("darkMode");
+  // var element = document.getElementById("darkModeButton");
+  // element.classList.toggle("hide");
+  // var element = document.getElementById("lightModeButton");
+  // element.classList.toggle("show");
+  var x = document.getElementById("darkModeButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("lightModeButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var element = document.body;
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("cButton", "backButton");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("backButton");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("percentButton");
+  element.classList.toggle("darkMode");
+    var element = document.getElementById("7Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("8Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("9Button");
+  element.classList.toggle("darkMode");
+      var element = document.getElementById("4Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("5Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("6Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("1Button");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("2Button");
+        element.classList.toggle("darkMode");
+        var element = document.getElementById("3Button");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("periodButton");
+  element.classList.toggle("darkMode");
+        var element = document.getElementById("zero");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("divideOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("timesOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("minusOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("addOperator");
+  element.classList.toggle("darkMode");
+          var element = document.getElementById("equaloOperator");
+  element.classList.toggle("darkModeEqual");
+  var element = document.getElementById("highlight");
+  element.classList.toggle("darkModeHighlight");
+  var element = document.getElementById("calculation");
+  element.classList.toggle("darkMode");
+  var element = document.getElementById("result");
+  element.classList.toggle("darkMode");
+  // var element = document.getElementById("darkModeButton");
+  // element.classList.toggle("show");
+  // var element = document.getElementById("lightModeButton");
+  // element.classList.toggle("hide");
+  var x = document.getElementById("darkModeButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("lightModeButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
